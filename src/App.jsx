60e06e322722fb8db77f7ff6fcff568592a09c42
Mitchell Genova <magenova12@gmail.com>
2020-09-08 12:16:07 -0400

    Added in user short cuts component
    Added in the user short cuts component that will display the avatar and the status of the user
    Did some more layout stuff to make the body of discord fit onto the page

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,22 @@ import React from 'react';
 import './App.scss';
 import Header from './Header';
 import Sidebar from './Sidebar';
+import UserShortcuts from './UserShortcuts';
 
 function App() {
   return (
     <div className="app">
       <Sidebar />
-      <div className="app__header">
-        <Header/>
-      </div>
-      <div className="app__body">
-        {/* React-Router -> Chat screen */}
+      <div className="app__container">
+        <div className="app__header">
+          <Header/>
+        </div>
+        <div className="app__body">
+          <div className="app__bodyLeft">
+            <UserShortcuts/>
+          </div>
+          {/* React-Router -> Chat screen */}
+        </div>
       </div>
     </div>
   );
--- src/UserShortcuts.jsx
+++ src/UserShortcuts.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import  { Avatar } from '@material-ui/core'
+import './UserShortcuts.scss';
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 
 
 function UserShortcuts() {
@@ -10,6 +12,7 @@ function UserShortcuts() {
         alt="Display name"
         src=""
       ></Avatar>
+      <FiberManualRecordIcon className="userShortcuts__status"/>
     </div>
   )
 }
