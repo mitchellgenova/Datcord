aa65a67104d84fd7c4e6bfffff9c200ba07ee983
Mitchell Genova <magenova12@gmail.com>
2020-10-06 22:38:24 -0400

    Unit testing setup and firebase setup
    Started setting up unit testing with jest and enzyme
    Setup the firebase stuff and pulled the servers down from the database

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Sidebar.scss';
+import db from './firebase';
+
 
 function Sidebar() {
+  const [servers, setServers] = useState([]);
+
+  useEffect(() => {
+    // Run this code ONCE when the sidebar component loads
+    db.collection('rooms').onSnapshot(snapshot => (
+      setServers(
+        snapshot.docs.map(doc => ({
+          id: doc.id,
+          name: doc.data().name,
+          serverImage: doc.data().serverImage,
+        })))
+    ));
+  }, [])
+
   return (
     <div className="sidebar">
       Channels go here
+      {servers.map(server => (
+        <div>
+          {/* {server.id} */}
+          {server.name}
+          <img src={server.serverImage}></img>
+        </div>
+      ))}
     </div>
   )
 }
---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB_vJJ6yYzv4NgWwTxuNvGauB65sTpbSqE",
+  authDomain: "datcord-140c3.firebaseapp.com",
+  databaseURL: "https://datcord-140c3.firebaseio.com",
+  projectId: "datcord-140c3",
+  storageBucket: "datcord-140c3.appspot.com",
+  messagingSenderId: "391032926323",
+  appId: "1:391032926323:web:af0b09ee6de8949d0bba06",
+  measurementId: "G-N91B4B4XN4"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
